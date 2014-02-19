63fb1434a144e377b336eabadb4af4ab4058dd84
Michael Berkovich <theiceberk@gmail.com>
2014-02-19 13:40:35 -0800

    switched to php

---
+++ includes/nav.php
@@ -0,0 +1,39 @@
+<?php tr8n_begin_block_with_options(array("source" => "navigation")) ?>
+
+<!-- Static navbar -->
+<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only"><?php tre("Toggle navigation") ?></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><strong>bookface</strong></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#"><?php tre("About Us") ?></a></li>
+                <li><a href="#"><?php tre("Newsfeed") ?></a></li>
+                <li><a href="#"><?php tre("Find Friends") ?></a></li>
+                <li><a href="#"><?php tre("Help") ?></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#" onClick="Tr8n.UI.LanguageSelector.show(true);">
+                        <img src="<?php echo tr8n_current_language()->flagUrl() ?>"> &nbsp;
+                        <?php echo tr8n_current_language()->english_name ?></a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php tre("More {caret}", array("caret" => '<b class="caret"></b>')) ?></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><?php tre("Log in") ?></a></li>
+                        <li><a href="#"><?php tre("Sign Up") ?></a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container-fluid -->
+</div>
+
+<?php tr8n_finish_block_with_options() ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php include('includes/header.php') ?>
+
+<?php include('includes/nav.php') ?>
+
+<div class="container">
+
+  <!-- Main component for a primary marketing message or call to action -->
+  <div class="jumbotron text-center">
+    <h1><?php tre("Share what's new in your life.") ?></h1>
+    <p class="lead text-muted"><?php tre("Connect with friends and the world around you on bookface.") ?></p>
+
+    <p>
+      <a class="btn btn-lg btn-success" href="../../components/#navbar" role="button"><?php tre("Create your free account") ?></a>
+    </p>
+  </div>
+
+</div> <!-- /container -->
+
+<?php include('includes/footer.php') ?>
+
